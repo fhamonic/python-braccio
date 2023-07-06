5f9915252065689486a0dba69abdc9d0663e3319
= <francois.hamonic@gmail.com>
2023-07-06 16:31:07 +0200

    clean up code + arduino serial communication

---
+++ arduino_script/arduino_script.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+#include "BraccioRobot.h"
+#define INPUT_BUFFER_SIZE 64
+
+static char inputBuffer[INPUT_BUFFER_SIZE];
+Position armPosition;
+
+void setup() {
+    Serial.begin(115200);
+    BraccioRobot.init(armPosition.set(0, 150, 0, 0, 90, 73));
+}
+
+void loop() { handleSerialInput(); }
+
+void handleSerialInput() {
+    if(!Serial.available()) return;
+    size_t byte_count =
+        Serial.readBytesUntil('\n', inputBuffer, INPUT_BUFFER_SIZE);
+    inputBuffer[byte_count] = '\0';
+    interpretCommand(inputBuffer, byte_count);
+}
+
+void interpretCommand(char * inputBuffer, byte commandLength) {
+    if(inputBuffer[0] == 'P') {
+        positionArm(inputBuffer + 1);
+    } else if(inputBuffer[0] == '0') {
+        BraccioRobot.powerOff();
+        Serial.println("OK");
+    } else if(inputBuffer[0] == '1') {
+        BraccioRobot.powerOn();
+        Serial.println("OK");
+    } else {
+        Serial.print("Unknown command: '");
+        Serial.print(inputBuffer);
+        Serial.println("'");
+    }
+    Serial.flush();
+}
+
+void positionArm(char * in) {
+    int speed = armPosition.setFromString(in);
+    if(speed < 0) {
+        Serial.println("Invalid position: '");
+        Serial.print(inputBuffer);
+        Serial.println("'");
+        return;
+    }
+    BraccioRobot.moveToPosition(armPosition, speed);
+    Serial.println("OK");
+}
--- braccio.py
+++ braccio.py
@@ -1,41 +1,65 @@
 import serial
 import time
-import solverNNA
 import numpy as np
 
 # sudo chmod 777 /dev/ttyACM0
 arm = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
-
 axis_names = ["base", "shoulder", "elbow", "wrist", "wristRot", "gripper"]
-home_angles = [0,150,0,0,90,73]
-min_max_angles = [(0,180), (15,165), (0,180), (0,180), (0,180), (0, 73)]
+min_max_angles = [(0, 180), (15, 165), (0, 180), (0, 180), (0, 180), (0, 73)]
 
-def clamp(value, bounds):
+
+def init():
+    print("INIT")
+    arm.readlines()  # wait for the arduino to wake up and empty the input buffer
+
+
+def wait_and_handle_ack():
+    response = arm.readline().decode().strip()
+    if response != "OK":
+        raise Exception("Arduino responded: '{}'".format(response))
+
+
+def power_on():
+    print("POWER_ON")
+    arm.write("1\n".encode())
+    wait_and_handle_ack()
+
+
+def power_off():
+    print("POWER_OFF")
+    arm.write("0\n".encode())
+    wait_and_handle_ack()
+
+
+def clamp_value(value, bounds):
     if value < bounds[0]:
         return bounds[0]
     if value > bounds[1]:
         return bounds[1]
     return value
 
-def wait_and_handle_ack():
-    response = arm.readline().decode().strip()
-    if response == "OK":
-        return
-    print("ERROR: '{}'".format(response))
 
-def write_angles(angles, speed=20):
-    angles = [clamp(angle, angle_bounds) for (angle, angle_bounds) in zip(angles, min_max_angles)]
-    angle_string="P {angles} {speed}\n".format(angles=' '.join([str(elem) for elem in angles]), speed=speed)
+def move_to(angles, speed=20):
+    print("MOVE_TO {} {}".format(angles, speed))
+    angles = [clamp_value(angle, angle_bounds)
+              for (angle, angle_bounds) in zip(angles, min_max_angles)]
+    angle_string = "P {angles} {speed}\n".format(
+        angles=' '.join([str(elem) for elem in angles]), speed=speed)
     arm.write(angle_string.encode())
-    arm.flush()
     wait_and_handle_ack()
 
+
 def home(speed=20):
-    write_angles(home_angles, speed)
+    print("HOME")
+    home_angles = [0, 150, 0, 0, 90, 73]
+    move_to(home_angles, speed)
 
-print("Initializing arm")
-time.sleep(2) # wait for the arduino to wake up
-arm.readlines() # empty the input buffer
 
-home()
-home()
+def shutdown(speed=50):
+    print("SHUTDOWN")
+    shutdown_angles = [0, 70, 20, 20, 90, 0]
+    move_to(shutdown_angles, speed)
+    power_off()
+
+init()
+shutdown()
