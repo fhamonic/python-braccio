7fa07f3eae400e08a3e4da9705d4fa01f24d0287
plaiseek <francois.hamonic@gmail.com>
2023-10-09 23:17:28 +0200

    work in progress

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -1,16 +1,75 @@
-#include <Servo.h>
-#include "BraccioRobot.h"
-#define INPUT_BUFFER_SIZE 64
+#include "TurboStepper.hpp"
 
+using Timer1 = ATMEGA328P::Timer1<C2MHz>;
+using Timer2 = ATMEGA328P::Timer2<C15_625Hz>;
+
+using Arm1Stepper = Stepper<10,9, 800, 4, 2>;
+using Arm2Stepper = Stepper<8,5, 800, 4, 2>;
+using ZStepper = Stepper<6,7, 1600, 1, 3>;
+
+using Arm1Profile = TrapezoidalProfile<Arm1Stepper, Timer1::CounterA>;
+using Arm2Profile = TrapezoidalProfile<Arm2Stepper, Timer1::CounterB>;
+using ZProfile = TrapezoidalProfile<ZStepper, Timer2::CounterA>;
+
+ISR(TIMER1_COMPA_vect) { Arm1Profile::DoStep(); }
+ISR(TIMER1_COMPB_vect) { Arm2Profile::DoStep(); }
+ISR(TIMER2_COMPA_vect) { ZProfile::DoStep(); }
+
+
+#define INPUT_BUFFER_SIZE 64
 static char inputBuffer[INPUT_BUFFER_SIZE];
-Position armPosition;
+
+
+// sudo chmod a+rw /dev/ttyACM0
+
 
 void setup() {
-    Serial.begin(115200);
-    BraccioRobot.init(armPosition.set(0, 30, 0, 0, 90, 73));
+    Arm1Stepper::Setup();
+    Arm2Stepper::Setup();
+    ZStepper::Setup();
+    
+    Timer1::Setup();
+    Timer2::Setup();
+    
+    Arm1Profile::Setup();
+    Arm2Profile::Setup();
+    ZProfile::Setup();
+
+    Serial.begin(19200);
+    pinModeFast(2, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() { handleSerialInput(); }
+
+
+void loop() { 
+  
+    ZProfile::SetSpeed(-1.0);
+    while(digitalReadFast(2) != LOW) delayMicroseconds(100);
+    ZProfile::SetSpeed(0.0);
+    while(ZProfile::IsMoving());
+    ZProfile::MoveForward(-ZStepper::stepPos);
+    
+    
+  while(true) {
+    
+    /*
+    Arm1Profile::MoveForward(1600);
+    Arm2Profile::MoveBackward(1600);
+    
+    ZProfile::MoveForward(4600);
+    
+    while(Arm1Profile::IsMoving() || Arm2Profile::IsMoving() || ZProfile::IsMoving());
+    Arm1Profile::MoveBackward(1600);
+    Arm2Profile::MoveForward(1600);
+    ZProfile::MoveBackward(4600);
+    while(Arm1Profile::IsMoving() || Arm2Profile::IsMoving() || ZProfile::IsMoving());
+*/
+    
+  }
+  //handleSerialInput(); 
+
+}
 
 void handleSerialInput() {
     if(!Serial.available()) return;
@@ -22,12 +81,9 @@ void handleSerialInput() {
 
 void interpretCommand(char * inputBuffer, byte commandLength) {
     if(inputBuffer[0] == 'P') {
-        positionArm(inputBuffer + 1);
     } else if(inputBuffer[0] == '0') {
-        BraccioRobot.powerOff();
         Serial.println("OK");
     } else if(inputBuffer[0] == '1') {
-        BraccioRobot.powerOn();
         Serial.println("OK");
     } else {
         Serial.print("Unknown command: '");
@@ -35,16 +91,4 @@ void interpretCommand(char * inputBuffer, byte commandLength) {
         Serial.println("'");
     }
     Serial.flush();
-}
-
-void positionArm(char * in) {
-    int speed = armPosition.setFromString(in);
-    if(speed < 0) {
-        Serial.println("Invalid position: '");
-        Serial.print(inputBuffer);
-        Serial.println("'");
-        return;
-    }
-    BraccioRobot.moveToPosition(armPosition, speed);
-    Serial.println("OK");
 }
